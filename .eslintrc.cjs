1083f725a817923a14827c0a4c518457a1feb4ba
Sebastian Mayer <sebastian.mayer@check24.de>
2024-02-04 16:21:31 +0100

    inital components and routing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,33 @@
+const { createBrowserRouter } = require("react-router-dom");
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: ["./tsconfig.json"]
   },
-}
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true }
+    ],
+    "no-unused-vars": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn"
+
+  }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui-components/Home.tsx";
+import Menu from "./features/menu/Menu.tsx";
+import Cart from "./features/cart/Cart.tsx";
+import CreateOrder from "./features/order/CreateOrder.tsx";
+import Order from "./features/order/Order.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  return <>Hello Vite</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
