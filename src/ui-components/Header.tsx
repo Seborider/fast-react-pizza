7a99d05480735ef3919a1fed69ed4dab31524214
Sebastian Mayer <sebastian.mayer@check24.de>
2024-02-12 23:15:18 +0100

    add tailwindcss

--- src/ui-components/Header.tsx
+++ src/ui-components/Header.tsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder.tsx";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder.tsx';
+import UserName from '../features/user/UserName.tsx';
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Sebo</p>
+      <UserName />
     </header>
   );
 }
